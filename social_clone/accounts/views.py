3057dfcb2f432187940452d1f8c937dd7436a909
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2018-02-27 19:45:02 +0000

    Finished & tested accounts app lesson. Debugged STATICFILES_DIRS reference.

--- social_clone/accounts/views.py
+++ social_clone/accounts/views.py
@@ -4,7 +4,11 @@ from accounts import forms
 from django.views.generic import CreateView
 
 # Create your views here.
+
+
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = "accounts/signup.html"
+
+
--- social_clone/social_clone/views.py
+++ social_clone/social_clone/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+
+
 class HomePage(TemplateView):
     template_name = "index.html"
+
+
+class TestPage(TemplateView):
+    template_name = "test.html"
+
+
+class ThanksPage(TemplateView):
+    template_name = "thanks.html"
